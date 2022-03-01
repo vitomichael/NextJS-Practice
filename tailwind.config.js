65fd6ec3f4fa89f377836af11c5d8c1c1b02728c
vitomichael <hanvitomichaellee@student.telkomuniversity.ac.id>
2022-03-01 21:38:36 +0700

    create a simple movie app

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: { screens: { "3xl": "2000px" } },
+  },
+  plugins: [require("tailwind-scrollbar-hide")],
+};
